af0534864b16f9945d20ae73d7105166829afb58
monicahillman <mmhillman1@ucs.br>
2024-02-23 21:39:15 -0300

    aula 02

--- src/paginas/cadastro/DadosPessoais.jsx
+++ src/paginas/cadastro/DadosPessoais.jsx
@@ -5,7 +5,7 @@ import { ListaSupensa } from "../../componentes/ListaSuspensa/ListaSuspensa"
 import { Col, Row } from "react-grid-system"
 import { Botao } from "../../componentes/Botao/Botao"
 import { Link } from "react-router-dom"
-import { Formik } from 'formik';
+import { Form, Formik } from 'formik';
 
 const estadosBrasileiros = [
     { "text": "Acre", "value": "AC" },
@@ -47,104 +47,100 @@ const DadosPessoais = () => {
             senha: '',
             confirmarSenha: ''
         }}>
-            <form>
-                <div style={{ textAlign: 'center' }}>
-                    <Tipografia variante="h1" componente="h1">
-                        Crie seu cadastro
-                    </Tipografia>
-                    <Tipografia variante="body" componente="body">
-                        Crie seu perfil gratuitamente para começar a trabalhar com os melhores freelancers. Em seguida, você poderá dar mais detalhes sobre suas demandas e sobre sua forma de trabalho.
-                    </Tipografia>
-                </div>
-                <Row>
-                    <Col>
-                        <CampoTexto
-                            titulo="Nome completo"
-                            valor={nome}
-                            onChange={setNome}
-                            tipo='text'
-                            required
-                        />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col lg={4} md={4} sm={4}>
-                        <ListaSupensa
-                            titulo="Estado"
-                            opcoes={estadosBrasileiros}
-                            valor={estado}
-                            onChange={setEstado}
-                        />
-                    </Col>
-                    <Col lg={8} md={8} sm={8}>
-                        <CampoTexto
-                            titulo="Cidade"
-                            valor={cidade}
-                            onChange={setCidade}
-                            tipo='text'
-                            required
-                        />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col lg={6} md={6} sm={6}>
-                        <CampoTexto
-                            titulo="E-mail"
-                            valor={email}
-                            onChange={setEmail}
-                            tipo='email'
-                            required
-                        />
-                    </Col>
-                    <Col lg={6} md={6} sm={6}>
-                        <CampoTexto
-                            titulo="Telefone"
-                            valor={telefone}
-                            onChange={setTelefone}
-                            tipo='tel'
-                            required
-                        />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col lg={6} md={6} sm={6}>
-                        <CampoTexto
-                            titulo="Senha"
-                            valor={senha}
-                            onChange={setSenha}
-                            tipo='password'
-                            required
-                        />
-                    </Col>
-                    <Col lg={6} md={6} sm={6}>
-                        <CampoTexto
-                            titulo="Confirme sua senha"
-                            valor={confirmarSenha}
-                            onChange={setConfirmarSenha}
-                            tipo='password'
-                            required
-                        />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col lg={6} md={6} sm={6}>
-                        <Link to="/cadastro/interesses">
-                            <Botao variante="secundaria">
-                                Anterior
-                            </Botao>
-                        </Link>
-                    </Col>
-                    <Col lg={6} md={6} sm={6}>
-                        <div style={{ textAlign: 'right' }}>
-                            {/* <Link to='/cadastro/concluido'> */}
-                            <Botao>
-                                Próxima
-                            </Botao>
-                            {/* </Link> */}
-                        </div>
-                    </Col>
-                </Row>
-            </form>
+            {formik => {
+                <Form onSubmit={formik.handleSubmit}>
+                    <div style={{ textAlign: 'center' }}>
+                        <Tipografia variante="h1" componente="h1">
+                            Crie seu cadastro
+                        </Tipografia>
+                        <Tipografia variante="body" componente="body">
+                            Crie seu perfil gratuitamente para começar a trabalhar com os melhores freelancers. Em seguida, você poderá dar mais detalhes sobre suas demandas e sobre sua forma de trabalho.
+                        </Tipografia>
+                    </div>
+                    <Row>
+                        <Col>
+                            <CampoTexto
+                                titulo="Nome completo"
+                                name='nome'
+                                type='text'
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={4} md={4} sm={4}>
+                            <ListaSupensa
+                                titulo="Estado"
+                                opcoes={estadosBrasileiros}
+                                valor={formik.values.estado}
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                            />
+                        </Col>
+                        <Col lg={8} md={8} sm={8}>
+                            <CampoTexto
+                                titulo="Cidade"
+                                name='cidade'
+                                type='text'
+                                required
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={6} md={6} sm={6}>
+                            <CampoTexto
+                                titulo="E-mail"
+                                name='email'
+                                type='email'
+                                required
+                            />
+                        </Col>
+                        <Col lg={6} md={6} sm={6}>
+                            <CampoTexto
+                                titulo="Telefone"
+                                name='telefone'
+                                type='tel'
+                                required
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={6} md={6} sm={6}>
+                            <CampoTexto
+                                titulo="Senha"
+                                name='senha'
+                                tipo='password'
+                                required
+                            />
+                        </Col>
+                        <Col lg={6} md={6} sm={6}>
+                            <CampoTexto
+                                titulo="Confirme sua senha"
+                                name='confirmarSenha'
+                                tipo='password'
+                                required
+                            />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={6} md={6} sm={6}>
+                            <Link to="/cadastro/interesses">
+                                <Botao variante="secundaria">
+                                    Anterior
+                                </Botao>
+                            </Link>
+                        </Col>
+                        <Col lg={6} md={6} sm={6}>
+                            <div style={{ textAlign: 'right' }}>
+                                {/* <Link to='/cadastro/concluido'> */}
+                                <Botao>
+                                    Próxima
+                                </Botao>
+                                {/* </Link> */}
+                            </div>
+                        </Col>
+                    </Row>
+                </Form>
+            }}
         </Formik>
 
     );
